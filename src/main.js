64ebdf0d7b070f27c57d8156cb763eeca90ac9d4
Juan Ramirez <juramirezp@bch.bancodechile.cl>
2023-10-11 13:01:47 -0300

    Update icons dependency

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from "vue";
 import "./style.css";
-import "/src/assets/new-icos-10-10-23/style.css";
+import "/node_modules/minerva-icons/index.css";
 import App from "./App.vue";
 
 createApp(App).mount("#app");
